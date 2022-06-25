7560723e2e00483b0b7576a2004e7c17a2500820
Brent Acs <brent.mann@gmai.com>
2022-06-25 18:58:28 -0400

    Working thru setting up a board model class that is abstract and a concrete 'standard' board. Board model is basic, and not constructed w/ anything other than null squares.

--- src/Apt.Chess.Game/Models/Board.cs
+++ src/Apt.Chess.Game/Models/Board.cs
@@ -1,12 +1,70 @@
 ﻿namespace Apt.Chess.Game;
 
+// public interface IChessBoard
+// {
+//    int MaxRank { get; }
+//    int MaxFile { get; }
+//
+//    Square GetSquare(ChessRank chessRank, ChessFile chessFile);
+// }
+//
+// public interface IStandardChessBoard : IChessBoard
+// {
+// }
+
+public interface IBoardModel
+{
+   Square[ , ]? Squares { get; set; }
+}
+
 /// <summary>
 /// Define a board by rank and file        :
 /// Vertical Columns, files, a-h (from white's left)
-/// Horizontal Rows, ranks, 1-2 (from white's side)
+/// Horizontal Rows, ranks, 1-8 (from white's side)
 /// </summary>
-public class Board
+public abstract class BoardModel : IBoardModel
 {
+   protected abstract int MaximumRank { get; }
+   protected abstract int MaximumFile { get; }
+   public Square[ , ]? Squares { get; set; }
+}
+
+public class StandardBoardModel : BoardModel
+{
+   private const int MaxRank = 8;
+   private const int MaxFile = 8;
+   
+   protected override int MaximumRank => MaxRank;
+   protected override int MaximumFile => MaxFile;
+
+   public StandardBoardModel()
+   {
+      Squares = new Square[ MaxRank, MaxFile ];
+   }
+}
+
+public enum ChessRank
+{
+   _1 = 0,
+   _2,
+   _3,
+   _4,
+   _5,
+   _6,
+   _7,
+   _8,
+}
+
+public enum ChessFile
+{
+   A = 0,
+   B,
+   C,
+   D,
+   E,
+   F,
+   G,
+   H,
 }
 
 /// <summary>
--- src/Apt.Chess.SimpleUI/Program.cs
+++ src/Apt.Chess.SimpleUI/Program.cs
@@ -1,3 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Apt.Chess.Game;
+using Apt.Chess.Game.Extensions;
+using Newtonsoft.Json;
+
 Console.WriteLine("Welcome to Apt Chess. There is nothing here yet. Enjoy the silence.");
+
+
+// var board = new StandardBoardModel();
+// var settings = new JsonSerializerSettings
+// {
+//    
+//    Formatting = Formatting.Indented,
+// };
+// var json = board.AsJsonIndented();
+// Console.WriteLine(json);
+
