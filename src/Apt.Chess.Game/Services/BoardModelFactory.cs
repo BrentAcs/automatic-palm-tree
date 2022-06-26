4149925693deb6d42065a11ea123cfd25df8c88c
Brent Acs <brent.mann@gmai.com>
2022-06-26 04:28:30 -0400

    Added CreateStock to board model factory to create a stock board. and improved the PoC UI just a tick.

--- src/Apt.Chess.Game/Services/BoardModelFactory.cs
+++ src/Apt.Chess.Game/Services/BoardModelFactory.cs
@@ -6,12 +6,15 @@ namespace Apt.Chess.Game.Services;
 public interface IBoardModelFactory
 {
    IBoardModel CreateEmpty();
-   IBoardModel Create(IDictionary<FileAndRank, Piece>? initialPieces=null);
+   IBoardModel CreateStock();
+   IBoardModel Create(IDictionary<FileAndRank, Piece>? initialPieces = null);
 }
 
 public abstract class BoardModelFactory : IBoardModelFactory
 {
+   protected abstract IDictionary<FileAndRank, Piece> InitialStockPieces { get; }
    protected abstract IBoardModel CreateEmptyBoard();
+
    protected virtual void PopulateInitialPieces(IBoardModel board, IDictionary<FileAndRank, Piece>? initialPieces = null)
    {
       if (initialPieces is null)
@@ -24,8 +27,11 @@ public abstract class BoardModelFactory : IBoardModelFactory
    }
 
    public IBoardModel CreateEmpty()
-      => Create(null);
-   
+      => Create();
+
+   public IBoardModel CreateStock()
+      => Create(InitialStockPieces);
+
    public IBoardModel Create(IDictionary<FileAndRank, Piece>? initialPieces = null)
    {
       var board = CreateEmptyBoard();
@@ -36,6 +42,47 @@ public abstract class BoardModelFactory : IBoardModelFactory
 
 public class StandardBoardModelFactory : BoardModelFactory
 {
+   protected override IDictionary<FileAndRank, Piece> InitialStockPieces =>
+      new Dictionary<FileAndRank, Piece>
+      {
+         // Black Back row
+         {new FileAndRank(ChessFile.A, ChessRank._8), new Piece(PieceType.Rook, ChessColor.Black)},
+         {new FileAndRank(ChessFile.B, ChessRank._8), new Piece(PieceType.Knight, ChessColor.Black)},
+         {new FileAndRank(ChessFile.C, ChessRank._8), new Piece(PieceType.Bishop, ChessColor.Black)},
+         {new FileAndRank(ChessFile.D, ChessRank._8), new Piece(PieceType.King, ChessColor.Black)},
+         {new FileAndRank(ChessFile.E, ChessRank._8), new Piece(PieceType.Queen, ChessColor.Black)},
+         {new FileAndRank(ChessFile.F, ChessRank._8), new Piece(PieceType.Bishop, ChessColor.Black)},
+         {new FileAndRank(ChessFile.G, ChessRank._8), new Piece(PieceType.Knight, ChessColor.Black)},
+         {new FileAndRank(ChessFile.H, ChessRank._8), new Piece(PieceType.Rook, ChessColor.Black)},
+         // Black Pawn row
+         {new FileAndRank(ChessFile.A, ChessRank._7), new Piece(PieceType.Pawn, ChessColor.Black)},
+         {new FileAndRank(ChessFile.B, ChessRank._7), new Piece(PieceType.Pawn, ChessColor.Black)},
+         {new FileAndRank(ChessFile.C, ChessRank._7), new Piece(PieceType.Pawn, ChessColor.Black)},
+         {new FileAndRank(ChessFile.D, ChessRank._7), new Piece(PieceType.Pawn, ChessColor.Black)},
+         {new FileAndRank(ChessFile.E, ChessRank._7), new Piece(PieceType.Pawn, ChessColor.Black)},
+         {new FileAndRank(ChessFile.F, ChessRank._7), new Piece(PieceType.Pawn, ChessColor.Black)},
+         {new FileAndRank(ChessFile.G, ChessRank._7), new Piece(PieceType.Pawn, ChessColor.Black)},
+         {new FileAndRank(ChessFile.H, ChessRank._7), new Piece(PieceType.Pawn, ChessColor.Black)},
+         // White Pawn row
+         {new FileAndRank(ChessFile.A, ChessRank._2), new Piece(PieceType.Pawn, ChessColor.White)},
+         {new FileAndRank(ChessFile.B, ChessRank._2), new Piece(PieceType.Pawn, ChessColor.White)},
+         {new FileAndRank(ChessFile.C, ChessRank._2), new Piece(PieceType.Pawn, ChessColor.White)},
+         {new FileAndRank(ChessFile.D, ChessRank._2), new Piece(PieceType.Pawn, ChessColor.White)},
+         {new FileAndRank(ChessFile.E, ChessRank._2), new Piece(PieceType.Pawn, ChessColor.White)},
+         {new FileAndRank(ChessFile.F, ChessRank._2), new Piece(PieceType.Pawn, ChessColor.White)},
+         {new FileAndRank(ChessFile.G, ChessRank._2), new Piece(PieceType.Pawn, ChessColor.White)},
+         {new FileAndRank(ChessFile.H, ChessRank._2), new Piece(PieceType.Pawn, ChessColor.White)},
+         // White Back row
+         {new FileAndRank(ChessFile.A, ChessRank._1), new Piece(PieceType.Rook, ChessColor.White)},
+         {new FileAndRank(ChessFile.B, ChessRank._1), new Piece(PieceType.Knight, ChessColor.White)},
+         {new FileAndRank(ChessFile.C, ChessRank._1), new Piece(PieceType.Bishop, ChessColor.White)},
+         {new FileAndRank(ChessFile.D, ChessRank._1), new Piece(PieceType.Queen, ChessColor.White)},
+         {new FileAndRank(ChessFile.E, ChessRank._1), new Piece(PieceType.King, ChessColor.White)},
+         {new FileAndRank(ChessFile.F, ChessRank._1), new Piece(PieceType.Bishop, ChessColor.White)},
+         {new FileAndRank(ChessFile.G, ChessRank._1), new Piece(PieceType.Knight, ChessColor.White)},
+         {new FileAndRank(ChessFile.H, ChessRank._1), new Piece(PieceType.Rook, ChessColor.White)}
+      };
+
    protected override IBoardModel CreateEmptyBoard()
    {
       var board = new StandardBoardModel();
--- src/Apt.Chess.SimpleUI/Program.cs
+++ src/Apt.Chess.SimpleUI/Program.cs
@@ -5,8 +5,50 @@ using Apt.Chess.Game.Services;
 
 // NOTE: Starting out, this will be VERY PoC. basically to visually show something after tests prove it.
 
+// var initialPieces = new Dictionary<FileAndRank, Piece>
+// {
+//    // {new FileAndRank(ChessFile.D, ChessRank._4), new Piece(PieceType.Pawn, ChessColor.White)}
+//
+//    // Black Back row
+//    {new FileAndRank(ChessFile.A, ChessRank._8), new Piece(PieceType.Rook, ChessColor.Black)},
+//    {new FileAndRank(ChessFile.B, ChessRank._8), new Piece(PieceType.Knight, ChessColor.Black)},
+//    {new FileAndRank(ChessFile.C, ChessRank._8), new Piece(PieceType.Bishop, ChessColor.Black)},
+//    {new FileAndRank(ChessFile.D, ChessRank._8), new Piece(PieceType.King, ChessColor.Black)},
+//    {new FileAndRank(ChessFile.E, ChessRank._8), new Piece(PieceType.Queen, ChessColor.Black)},
+//    {new FileAndRank(ChessFile.F, ChessRank._8), new Piece(PieceType.Bishop, ChessColor.Black)},
+//    {new FileAndRank(ChessFile.G, ChessRank._8), new Piece(PieceType.Knight, ChessColor.Black)},
+//    {new FileAndRank(ChessFile.H, ChessRank._8), new Piece(PieceType.Rook, ChessColor.Black)},
+//    // Black Pawn row
+//    {new FileAndRank(ChessFile.A, ChessRank._7), new Piece(PieceType.Pawn, ChessColor.Black)},
+//    {new FileAndRank(ChessFile.B, ChessRank._7), new Piece(PieceType.Pawn, ChessColor.Black)},
+//    {new FileAndRank(ChessFile.C, ChessRank._7), new Piece(PieceType.Pawn, ChessColor.Black)},
+//    {new FileAndRank(ChessFile.D, ChessRank._7), new Piece(PieceType.Pawn, ChessColor.Black)},
+//    {new FileAndRank(ChessFile.E, ChessRank._7), new Piece(PieceType.Pawn, ChessColor.Black)},
+//    {new FileAndRank(ChessFile.F, ChessRank._7), new Piece(PieceType.Pawn, ChessColor.Black)},
+//    {new FileAndRank(ChessFile.G, ChessRank._7), new Piece(PieceType.Pawn, ChessColor.Black)},
+//    {new FileAndRank(ChessFile.H, ChessRank._7), new Piece(PieceType.Pawn, ChessColor.Black)},
+//    // White Pawn row
+//    {new FileAndRank(ChessFile.A, ChessRank._2), new Piece(PieceType.Pawn, ChessColor.White)},
+//    {new FileAndRank(ChessFile.B, ChessRank._2), new Piece(PieceType.Pawn, ChessColor.White)},
+//    {new FileAndRank(ChessFile.C, ChessRank._2), new Piece(PieceType.Pawn, ChessColor.White)},
+//    {new FileAndRank(ChessFile.D, ChessRank._2), new Piece(PieceType.Pawn, ChessColor.White)},
+//    {new FileAndRank(ChessFile.E, ChessRank._2), new Piece(PieceType.Pawn, ChessColor.White)},
+//    {new FileAndRank(ChessFile.F, ChessRank._2), new Piece(PieceType.Pawn, ChessColor.White)},
+//    {new FileAndRank(ChessFile.G, ChessRank._2), new Piece(PieceType.Pawn, ChessColor.White)},
+//    {new FileAndRank(ChessFile.H, ChessRank._2), new Piece(PieceType.Pawn, ChessColor.White)},
+//    // White Back row
+//    {new FileAndRank(ChessFile.A, ChessRank._1), new Piece(PieceType.Rook, ChessColor.White)},
+//    {new FileAndRank(ChessFile.B, ChessRank._1), new Piece(PieceType.Knight, ChessColor.White)},
+//    {new FileAndRank(ChessFile.C, ChessRank._1), new Piece(PieceType.Bishop, ChessColor.White)},
+//    {new FileAndRank(ChessFile.D, ChessRank._1), new Piece(PieceType.Queen, ChessColor.White)},
+//    {new FileAndRank(ChessFile.E, ChessRank._1), new Piece(PieceType.King, ChessColor.White)},
+//    {new FileAndRank(ChessFile.F, ChessRank._1), new Piece(PieceType.Bishop, ChessColor.White)},
+//    {new FileAndRank(ChessFile.G, ChessRank._1), new Piece(PieceType.Knight, ChessColor.White)},
+//    {new FileAndRank(ChessFile.H, ChessRank._1), new Piece(PieceType.Rook, ChessColor.White)}
+// };
+
 var board = new StandardBoardModelFactory()
-   .Create();
+   .CreateStock();
 
 // NOTE: this is VERY tacky and will be heavily refactored and/or thrown out.
 const int boardTop = 1;
@@ -19,24 +61,44 @@ foreach (var file in Enum.GetValues<ChessFile>())
       var top = boardTop + (21 - (int)rank * 3);
 
       var square = board[ file, rank ];
-      Console.BackgroundColor = square.SquareColor == ChessColor.Black ? ConsoleColor.Black : ConsoleColor.White;
-      Console.ForegroundColor = square.SquareColor == ChessColor.Black ? ConsoleColor.White : ConsoleColor.Black;
+      Console.BackgroundColor = square.SquareColor == ChessColor.Black ? ConsoleColor.DarkGray : ConsoleColor.Gray;
 
       Console.CursorLeft = boardLeft + (int)file * 5;
       Console.CursorTop = top;
-      Console.WriteLine("     ");
+      Console.Write("     ");
 
       Console.CursorLeft = boardLeft + (int)file * 5;
       Console.CursorTop = top + 1;
-      Console.WriteLine("     ");
+      Console.Write("     ");
+      if (square.Piece is not null)
+      {
+         Console.CursorLeft -= 3;
+         Console.ForegroundColor = square.Piece.Player == ChessColor.Black ? ConsoleColor.Black : ConsoleColor.White;
+         Console.Write($"{square.Piece.Type.ToDisplay()}");
+      }
 
       Console.CursorLeft = boardLeft + (int)file * 5;
       Console.CursorTop = top + 2;
-      Console.WriteLine("     ");
+      Console.Write("     ");
    }
 }
 
 Console.ReadKey();
+
+public static class PieceTypeExtensions
+{
+   public static string ToDisplay(this PieceType type) =>
+      type switch
+      {
+         PieceType.King => "K",
+         PieceType.Queen => "Q",
+         PieceType.Rook => "R",
+         PieceType.Bishop => "B",
+         PieceType.Knight => "N",
+         PieceType.Pawn => "p",
+         _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+      };
+}
 
 
 // var board = new StandardBoardModel();
