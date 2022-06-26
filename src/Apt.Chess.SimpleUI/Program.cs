a523b5d01151c3aa397816ba7ecfe6784e91f955
Brent Acs <brent.mann@gmai.com>
2022-06-25 21:23:44 -0400

    added very simple, crude board display for empty.

--- src/Apt.Chess.SimpleUI/Program.cs
+++ src/Apt.Chess.SimpleUI/Program.cs
@@ -1,13 +1,42 @@
-﻿using Apt.Chess.Game.Services;
+﻿using Apt.Chess.Game;
+using Apt.Chess.Game.Services;
 
-Console.WriteLine("Welcome to Apt Chess. There is nothing here yet. Enjoy the silence.");
+// Console.WriteLine("Welcome to Apt Chess. There is nothing here yet. Enjoy the silence.");
 
 // NOTE: Starting out, this will be VERY PoC. basically to visually show something after tests prove it.
 
 var board = new StandardBoardModelFactory()
    .Create();
 
- 
+const int boardTop = 1;
+const int boardLeft = 10;
+
+foreach (var file in Enum.GetValues<ChessFile>())
+{
+   foreach (var rank in Enum.GetValues<ChessRank>())
+   {
+      var top = boardTop + (21 - (int)rank * 3);
+
+      var square = board[ file, rank ];
+      Console.BackgroundColor = square.SquareColor == ChessColor.Black ? ConsoleColor.Black : ConsoleColor.White;
+      Console.ForegroundColor = square.SquareColor == ChessColor.Black ? ConsoleColor.White : ConsoleColor.Black;
+
+      Console.CursorLeft = boardLeft + (int)file * 5;
+      Console.CursorTop = top;
+      Console.WriteLine("     ");
+
+      Console.CursorLeft = boardLeft + (int)file * 5;
+      Console.CursorTop = top + 1;
+      Console.WriteLine("     ");
+
+      Console.CursorLeft = boardLeft + (int)file * 5;
+      Console.CursorTop = top + 2;
+      Console.WriteLine("     ");
+   }
+}
+
+Console.ReadKey();
+
 
 // var board = new StandardBoardModel();
 // var settings = new JsonSerializerSettings
@@ -17,4 +46,3 @@ var board = new StandardBoardModelFactory()
 // };
 // var json = board.AsJsonIndented();
 // Console.WriteLine(json);
-
